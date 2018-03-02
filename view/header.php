b20f401286be68fa0eaa90539fb47eb525f2b25c
Max Xu <xujialv@gmail.com>
2018-03-02 16:28:58 -0500

    Update header.php

--- view/header.php
+++ view/header.php
@@ -6,7 +6,7 @@
     <title>SportsPro Technical Support</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css"
-          href="http://webdev.bentley.edu/xujial/assign3/main.css">
+          href="https://webdev.bentley.edu/xujial/assign3/main.css">
 
 </head>
 
